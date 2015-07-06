7cf1236a6e54af99624c42a3d9be2c51f2ada645
ashvinisharma <ashv@live.in>
2015-07-06 21:17:21 +0530

    Update/changes in framework

---
+++ SeleniumHybridFramework/src/main/java/com/selenium/utility/XMLparsing.java
@@ -0,0 +1,16 @@
+package com.selenium.utility;
+
+import org.apache.log4j.Logger;
+
+public class XMLparsing {
+	
+	/*
+	 * Start logging details.
+	 */
+	static Logger log = Logger.getLogger(XMLparsing.class.getName());
+	static final String LOG_PROPERTIES_FILE = "Configuration/log.properties";
+	
+	
+	
+}
+
