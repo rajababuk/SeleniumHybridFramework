b276cd8499594b5018265e5fdf73f855977ca017
ashvinisharma <ashv@live.in>
2015-07-06 22:34:14 +0530

    Update Framework and comments in files.

---
+++ SeleniumHybridFramework/src/main/java/com/selenium/utility/ReadProjectConfiguration.java
@@ -0,0 +1,40 @@
+/*************************************************************************************
+ *
+ * 	Copyright (C) 2015 Ashvini Sharma
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the 
+ *  License is distributed on an "AS IS" BASIS,WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ *  either express or implied. See the License for the specific language governing permissions 
+ *  and limitations under the License.
+ *  
+ ******************************************************************************/
+
+
+package com.selenium.utility;
+
+
+
+
+
+/**
+ * This class is used to read configuration details, defined in file /resources/Project.configuration.
+ * This file will be called only once during the entire suite run. 
+ * This class object is used by main/suit runner file, which is under model package. 
+ * 
+ * @author ashvini sharma
+ * @version 1.0
+ * 
+ * Last Edited by ashvini sharma on @date 6-June-2015
+ * @version 1.0
+ *
+ */
+
+public class ReadProjectConfiguration {
+
+}
